688a86488af0e0df1259a958cb15f378ad2a71d9
Rafael Strack <strackinado@gmail.com>
2022-06-17 14:26:04 -0300

    commit 2

---
+++ src/main/java/dw/futebol/model/Jogador.java
@@ -0,0 +1,58 @@
+package dw.futebol.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "jogador")
+public class Jogador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long cod_jogador;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String email;
+
+    @Column
+    private Date datanasc;
+
+    public Jogador(String nome, String email, Date datanasc){
+        this.nome=nome;
+        this.email=email;
+        this.datanasc=datanasc;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDatanasc() {
+        return this.datanasc;
+    }
+
+    public void setDatanasc(Date datanasc) {
+        this.datanasc = datanasc;
+    }
+
+}
---
+++ src/main/java/dw/futebol/model/Pagamento.java
@@ -0,0 +1,61 @@
+package dw.futebol.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pagamento")
+public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long cod_pagamento;
+
+    @Column
+    private short ano;
+
+    @Column
+    private short mes;
+
+    @Column
+    private BigDecimal valor;
+
+    public Pagamento(short ano, short mes, BigDecimal valor){
+        this.ano = ano;
+        this.mes=mes;
+        this.valor=valor;
+    }
+
+    public short getAno() {
+        return this.ano;
+    }
+
+    public void setAno(short ano) {
+        this.ano = ano;
+    }
+
+    public short getMes() {
+        return this.mes;
+    }
+
+    public void setMes(short mes) {
+        this.mes = mes;
+    }
+
+    public BigDecimal getValor() {
+        return this.valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+
+
+
+}
